258fbf9df9f3ba39ce6c23b8010a43627a0a3508
Alisha Dhir <alishadhir@Second-Gina.fios-router.home>
2020-04-15 23:29:08 -0400

    woohoo new but similar idea I am excite

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# stayinsidetogether
-a chrome extension as a response to corona times
+# trying is winning
+
+a chrome extension as a response to corona times; your response to quarantine is valid, no matter what it is. Trying is winning, no matter what you try or how far you get.
--- content.js
+++ content.js
@@ -1,4 +1,22 @@
-var ptag = document.getElementsByTagName("list");
-for (var i = 0, l = ptag.length; i < l; i++) {
-  
+// thank u dan shiffman u are a true king
+// also melanie xox
+console.log("Chrome extension is running!");
+
+// Listen for messages
+chrome.runtime.onMessage.addListener(receiver);
+
+// A message is received
+function receiver(request, sender, sendResponse) {
+
+  // Grab every single DOM element
+  var elts = document.getElementsByTagName('*');
+
+
+  // // Change the background color and font-size
+  // // according to the message
+  // for (var i = 0; i < elts.length; i++) {
+  //   elts[i].style['background-color'] = request.color;
+  //   elts[i].style['font-size'] = request.size + '%';
+  }
+
 }
